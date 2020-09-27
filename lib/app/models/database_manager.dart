6320e0b5407898b86734661e5eb6ea897e1857c4
ukkey0518 <ukitto0518@gmail.com>
2020-09-27 17:39:13 +0900

    パッケージの更新

--- lib/app/models/database_manager.dart
+++ lib/app/models/database_manager.dart
@@ -2,38 +2,38 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sky_score_generator/app/models/model_classes/score.dart';
 
 class DatabaseManager {
-  final _scoreCollection = Firestore.instance.collection('score');
+  final _scoreCollection = FirebaseFirestore.instance.collection('score');
 
   Future<void> saveScore(Score score) async {
-    await _scoreCollection.document(score.id).setData(score.toMap());
+    await _scoreCollection.doc(score.id).set(score.toMap());
   }
 
   Future<List<Score>> getAllScores() async {
-    final snapshot = await _scoreCollection.getDocuments();
+    final snapshot = await _scoreCollection.get();
 
-    if (snapshot.documents.isEmpty) {
+    if (snapshot.docs.isEmpty) {
       return <Score>[];
     }
 
     final scores = <Score>[];
-    snapshot.documents.forEach((document) {
-      scores.add(Score.fromMap(document.data));
+    snapshot.docs.forEach((document) {
+      scores.add(Score.fromMap(document.data()));
     });
 
     return scores;
   }
 
   Future<Score> getScoreById(String scoreId) async {
-    final document = await _scoreCollection.document(scoreId).get();
+    final document = await _scoreCollection.doc(scoreId).get();
 
     if (document == null || document.data == null) {
       return null;
     }
 
-    return Score.fromMap(document.data);
+    return Score.fromMap(document.data());
   }
 
   Future<void> deleteScore(String scoreId) async {
-    await _scoreCollection.document(scoreId).delete();
+    await _scoreCollection.doc(scoreId).delete();
   }
 }
